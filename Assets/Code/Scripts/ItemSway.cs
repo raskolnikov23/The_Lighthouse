bb47228471a1c2e005ec3fb5ee4cea0cde9a0a7d
raskolnikov23 <107072445+raskolnikov23@users.noreply.github.com>
2023-09-03 02:25:26 +0300

    base implementation of item swaying while moving

--- Assets/Code/Scripts/ItemSway.cs
+++ Assets/Code/Scripts/ItemSway.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ItemSway : MonoBehaviour
@@ -7,7 +6,28 @@ public class ItemSway : MonoBehaviour
 
     public GameObject parent;
     public ItemHandler itemHandler;
+    public PlayerMovement playerMovement;
     public int swayState;
+
+    public Vector3 standardPos;
+    public Vector3 middlePos;
+    public Vector3 leftPos;
+    public Vector3 rightPos;
+    public Vector3 newPos;
+
+    public float xOffsetL;
+    public float xOffsetR;
+    public float yOffset;
+
+    public Vector3 _ref;
+
+    public float swayTime;
+
+    public float timer;
+    public float timerValue;
+
+    public int direction;
+    public float normalizeTime;
 
     void Update()
     {
@@ -19,6 +39,58 @@ public class ItemSway : MonoBehaviour
 
     private void Sway()
     {
+
+        if (playerMovement.walking) 
+        {
+            standardPos = itemHandler.currentlyEquippedItem.GetComponent<ItemInstance>().itemData.equipLocation;
+            leftPos = new Vector3(standardPos.x + xOffsetL, standardPos.y + yOffset, standardPos.z);
+            rightPos = new Vector3(standardPos.x + xOffsetR, standardPos.y + yOffset, standardPos.z);
+            middlePos = new Vector3(standardPos.x, standardPos.y, standardPos.z);
+
+            itemHandler.currentlyEquippedItem.transform.localPosition = Vector3.SmoothDamp(itemHandler.currentlyEquippedItem.transform.localPosition, newPos, ref _ref, swayTime);
+
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                SwitchPos();
+            }
+
+        }
+        else if (playerMovement.standing)
+        {
+            itemHandler.currentlyEquippedItem.transform.localPosition = Vector3.SmoothDamp(itemHandler.currentlyEquippedItem.transform.localPosition, standardPos, ref _ref, normalizeTime);
+        }
+
+        // if standing
+
+
         //itemHandler.currentlyEquippedItem.transform.localPosition = Vector3.SmoothDamp()
+    }
+
+    public void SwitchPos()
+    {
+        if (swayState == -1 || swayState == 1)
+        {
+            newPos = middlePos;
+            swayState = 0;
+            direction *= -1;
+        }
+
+        else if (swayState == 0)
+        {
+            if (direction == -1)
+            {
+                newPos = leftPos;
+                swayState = -1;
+            }
+            if (direction == 1)
+            {
+                newPos = rightPos;
+                swayState = 1;
+            }
+        }
+
+        timer = timerValue;
     }
 }
